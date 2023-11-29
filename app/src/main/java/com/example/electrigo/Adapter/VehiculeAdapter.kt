1ed02801e0f9e7049478e11064dfb885ffe147fd
ademnsir <nsir.adem@esprit.tn>
2023-11-29 15:02:45 +0100

    Somechanges

---
+++ app/src/main/java/com/example/electrigo/Adapter/VehiculeAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.electrigo.Adapter
+
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.electrigo.Model.Vehicule
+import com.example.electrigo.R
+
+class VehiculeAdapter(private val vehicules: ArrayList<Vehicule>) :
+    RecyclerView.Adapter<VehiculeAdapter.VehiculeViewHolder>() {
+
+    class VehiculeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageViewVehicule)
+        val textViewMarque: TextView = itemView.findViewById(R.id.textViewMarque)
+        val textViewPrix: TextView = itemView.findViewById(R.id.Textprice)
+        val textViewnombreDePlaces: TextView = itemView.findViewById(R.id.textNbredeplaces)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehiculeViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_vehicule, parent, false)
+        return VehiculeViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: VehiculeViewHolder, position: Int) {
+        val vehicule = vehicules[position]
+        holder.imageView.setImageResource(vehicule.image)
+        holder.textViewMarque.text = vehicule.marque
+        holder.textViewPrix.text = vehicule.prix
+        holder.textViewnombreDePlaces.text = vehicule.nombreDePlaces.toString()
+    }
+
+
+    override fun getItemCount(): Int {
+        return vehicules.size
+    }
+}
