7a3ad9a3cf11b0a3b54ca515b0ed10f7db560fc6
aziz jazzar <jazzar.aziz@esprit.tn>
2023-11-30 21:13:38 +0100

    ajout consomation +controle de saisie

--- app/src/main/java/com/example/electrigo/ViewModel/RegisterActivity.kt
+++ app/src/main/java/com/example/electrigo/ViewModel/RegisterActivity.kt
@@ -33,26 +33,63 @@ class RegisterActivity : AppCompatActivity() {
             val selectedRadioButtonId = binding.radioGroup.checkedRadioButtonId
             val radioButton = findViewById<RadioButton>(selectedRadioButtonId)
             val selectedValue = radioButton?.text.toString()
-            val user = User(
-                binding.tiNom.text.toString(),
-                binding.tiPrenom.text.toString(),
-                binding.tiEmail.text.toString(),
-                // Assuming radioGroup has a function to get the selected value
-                selectedValue,
-                binding.tiDateNaissance.text.toString(),
-                binding.tiTelephone.text.toString(),
-                binding.tiadresse.text.toString(),
-                binding.tiMotPasse.text.toString(),
-                "client",
-                "Default"
+
+            val nom = binding.tiNom.text.toString()
+            val prenom = binding.tiPrenom.text.toString()
+            val email = binding.tiEmail.text.toString()
+            val dateNaissance = binding.tiDateNaissance.text.toString()
+            val telephone = binding.tiTelephone.text.toString()
+            val adresse = binding.tiadresse.text.toString()
+            val motPasse = binding.tiMotPasse.text.toString()
+            val confirmMotPasse = binding.tiConfirmationMotPasse.text.toString()
+
+            // Perform input validation
+            val validationErrors = validateInput(
+                nom, prenom, email, dateNaissance, telephone, adresse, motPasse, confirmMotPasse
             )
 
-            CoroutineScope(Dispatchers.Main).launch {
-                ajouterUtilisateur(user)
+            if (validationErrors.isEmpty()) {
+                val user = User(
+                    nom, prenom, email, selectedValue,
+                    dateNaissance, telephone, adresse, motPasse,
+                    "client", "Default"
+                )
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    ajouterUtilisateur(user)
+                }
+            } else {
+                showAlert(validationErrors.joinToString("\n"))
             }
         }
-    }
 
+
+
+
+
+    }
+    // Function to check if input is valid
+    private fun isInputValid(
+        nom: String,
+        prenom: String,
+        email: String,
+        dateNaissance: String,
+        telephone: String,
+        adresse: String,
+        motPasse: String,
+        confirmMotPasse: String
+    ): Boolean {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+        return nom.isNotBlank() &&
+                prenom.isNotBlank() &&
+                email.isNotBlank() && email.matches(emailPattern.toRegex()) &&
+                dateNaissance.isNotBlank() &&
+                telephone.isNotBlank() && telephone.length == 8 &&
+                adresse.isNotBlank() &&
+                motPasse.isNotBlank() && motPasse.length > 8 &&
+                motPasse == confirmMotPasse
+    }
     fun showDatePicker(view: View?) {
         val dateSetListener =
             DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
@@ -68,7 +105,54 @@ class RegisterActivity : AppCompatActivity() {
         val datePickerDialog = DatePickerDialog(this, dateSetListener, year, month, day)
         datePickerDialog.show()
     }
+    // Function to validate input
+    private fun validateInput(
+        nom: String,
+        prenom: String,
+        email: String,
+        dateNaissance: String,
+        telephone: String,
+        adresse: String,
+        motPasse: String,
+        confirmMotPasse: String
+    ): List<String> {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        val errors = mutableListOf<String>()
 
+        if (nom.isBlank()) {
+            errors.add("Le champ Nom est obligatoire.")
+        }
+
+        else if (prenom.isBlank()) {
+            errors.add("Le champ Prénom est obligatoire.")
+        }
+
+        else if (email.isBlank() || !email.matches(emailPattern.toRegex())) {
+            errors.add("Veuillez saisir une adresse e-mail valide.")
+        }
+
+        else if (dateNaissance.isBlank()) {
+            errors.add("Le champ Date de Naissance est obligatoire.")
+        }
+
+        else if (telephone.isBlank() || telephone.length != 8) {
+            errors.add("Veuillez saisir un numéro de téléphone valide (8 chiffres).")
+        }
+
+        else if (adresse.isBlank()) {
+            errors.add("Le champ Adresse est obligatoire.")
+        }
+
+        else if (motPasse.isBlank() || motPasse.length <= 8) {
+            errors.add("Le mot de passe doit contenir plus de 8 caractères.")
+        }
+
+        else if (motPasse != confirmMotPasse) {
+            errors.add("Les mots de passe ne correspondent pas.")
+        }
+
+        return errors
+    }
     private suspend fun ajouterUtilisateur(user: User) {
 
             try {
